5f94e4a80839a19ef39bc29fde4bacf4505363bc
Asif <asifreja74@gmail.com>
2025-12-13 15:39:28 +0530

    first commit

---
+++ src/test/java/com/asif/sweet_shop_backend/SweetShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asif.sweet_shop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
